f02ca9a1cc492f40409077fd7500542d9ddc7e1e
i3d <i3dmaster@gmail.com>
2021-05-03 23:26:18 -0700

    auto update lsp config

--- vim.mac13/init.lua
+++ vim.mac13/init.lua
@@ -256,10 +256,16 @@ telescope_t.setup {
 }
 
 -- lspinstall
-require'lspinstall'.setup() -- important
-local servers = require'lspinstall'.installed_servers()
-for _, server in pairs(servers) do
-  require'lspconfig'[server].setup{}
+local function setup_servers()
+  require'lspinstall'.setup()
+  local servers = require'lspinstall'.installed_servers()
+  for _, server in pairs(servers) do
+    require'lspconfig'[server].setup{}
+  end
 end
--- this should no longer needed.
--- require'lspconfig'.rust_analyzer.setup({})
+setup_servers() -- important
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+end
