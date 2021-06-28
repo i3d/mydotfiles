be5b5ed2c40047d076c5fb45cde493f83e7d8712
i3d <i3dmaster@gmail.com>
2021-06-27 17:57:13 -0700

    make on_attach to all lsp

--- vim.mac13/init.lua
+++ vim.mac13/init.lua
@@ -255,25 +255,6 @@ telescope_t.setup {
   },
 }
 
--- lspinstall
-local function setup_servers()
-  require'lspinstall'.setup()
-  local servers = require'lspinstall'.installed_servers()
-  for _, server in pairs(servers) do
-    require'lspconfig'[server].setup{}
-  end
-end
-setup_servers() -- important
--- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-require'lspinstall'.post_install_hook = function ()
-  setup_servers() -- reload installed servers
-  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-end
-
--- https://sharksforarms.dev/posts/neovim-rust/
--- nvim_lsp object
-local nvim_lsp = require'lspconfig'
-
 -- function to attach completion when setting up lsp
 local on_attach = function(client)
   require'completion'.on_attach(client)
@@ -286,6 +267,26 @@ local on_attach = function(client)
       },
   })
 end
+
+-- lspinstall
+local function setup_servers()
+  require'lspinstall'.setup()
+  local servers = require'lspinstall'.installed_servers()
+  for _, server in pairs(servers) do
+    require'lspconfig'[server].setup{ on_attach=on_attach }
+  end
+end
+setup_servers() -- important
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+end
+
+-- https://sharksforarms.dev/posts/neovim-rust/
+-- nvim_lsp object
+local nvim_lsp = require'lspconfig'
 
 -- Enable rust_analyzer
 nvim_lsp.rust_analyzer.setup({ on_attach=on_attach })
