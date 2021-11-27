a42e55e9a70fd62d92d18233f493707b74060e84
i3d <i3dmaster@gmail.com>
2021-11-26 22:41:58 -0800

    rename other configs also from quitedark to carbondark

--- vim.mac13/lua/plugs.lua
+++ vim.mac13/lua/plugs.lua
@@ -1,8 +1,8 @@
 -- vim.cmd("colorscheme darkmatrix")
-vim.cmd("colorscheme quitedark")
+vim.cmd("colorscheme carbondark")
 
 if string.match(vim.g.colors_name, "matrix") then require('plugins.matrix') end
-if string.match(vim.g.colors_name, "quitedark") then require('plugins.quitedark') end
+if string.match(vim.g.colors_name, "carbondark") then require('plugins.carbondark') end
 -- require('plugins.onedark')
 -- require('plugins.nerd')
 -- require('plugins.light')
