3444d7796bacc1cc358782371ea82d3946770aba
i3d <i3dmaster@gmail.com>
2024-01-26 18:34:43 -0800

    add matrix color for wezterm

---
+++ wezterm/colors/matrix.lua
@@ -0,0 +1,32 @@
+local colorscheme = {
+  foreground = "#426644",
+  background = "#091013",
+  cursor_bg = "#384545",
+  cursor_border = "#00ff00",
+  cursor_fg = "#00ff00",
+  selection_bg = "#18282e",
+  selection_fg = "#00ff87",
+
+  ansi = {
+    "#091013",
+    "#23755a",
+    "#82d967",
+    "#ffd700",
+    "#3f5242",
+    "#309931",
+    "#50b45a",
+    "#507350",
+  },
+  brights = {
+    "#333333",
+    "#2fc079",
+    "#90d762",
+    "#faff00",
+    "#4f7e7e",
+    "#11ff25",
+    "#c1ff8a",
+    "#678c61",
+  },
+}
+
+return colorscheme
