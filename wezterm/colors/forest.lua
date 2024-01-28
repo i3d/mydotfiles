e18249332ad936f7f7dd078685b6becbd9baffd8
i3d <i3dmaster@gmail.com>
2024-01-27 17:12:18 -0800

    add forst color for wezterm

---
+++ wezterm/colors/forest.lua
@@ -0,0 +1,32 @@
+local colorscheme = {
+  foreground = "#9eb19c",
+  background = "#161b20",
+  cursor_bg = "#3b403a",
+  cursor_border = "#00ff87",
+  cursor_fg = "#c5dc58",
+  selection_bg = "#b0c44a",
+  selection_fg = "#151b22",
+
+  ansi = {
+    "#b0b400",
+    "#a4b259",
+    "#c5dc58",
+    "#bee14d",
+    "#412a4d",
+    "#b0c44a",
+    "#b0c44a",
+    "#1d201d",
+  },
+  brights = {
+    "#acbf4c",
+    "#bcd053",
+    "#def475",
+    "#c4eda0",
+    "#412a4d",
+    "#90d762",
+    "#b0c44a",
+    "#212321",
+  },
+}
+
+return colorscheme
