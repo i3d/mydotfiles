9fd6524311570dcea2ea3263cbaa556aa2b03784
i3d <i3dmaster@gmail.com>
2024-02-11 18:21:49 -0800

    add matrix color config

---
+++ wezterm/colors/carbon.lua
@@ -0,0 +1,32 @@
+local colorscheme = {
+  foreground = '#4B4C4B',
+  background = "#101010",
+  cursor_bg = "#575A3B",
+  cursor_border = "#00ff87",
+  cursor_fg = "#e6e6e6",
+  selection_bg = "#111111",
+  selection_fg = "#e6e6e6",
+
+  ansi = {
+    "#575757",
+    "#81b32c",
+    "#b39700",
+    "#848484",
+    "#412a4d",
+    "#7D7D7D",
+    "#7D7D7D",
+    "#101010",
+  },
+  brights = {
+    "#666666",
+    "#81b32c",
+    "#b39700",
+    "#fafafa",
+    "#412a4d",
+    "#9f9f9f",
+    "#e6e6e6",
+    "#404040",
+  },
+}
+
+return colorscheme
---
+++ wezterm/colors/kanagawa.lua
@@ -0,0 +1,33 @@
+local colorscheme = {
+  foreground = "#DCD7BA",
+  background = "#1F1F28",
+  cursor_bg = "#c0caf5",
+  cursor_border = "#c0caf5",
+  cursor_fg = "#C8C093",
+  selection_bg = "#2D4F67",
+  selection_fg = "#C8C093",
+
+  ansi = {
+    "#16161D",
+    "#BB9BF7",
+    "#76946A",
+    "#C0A36E",
+    "#7E9CD8",
+    "#957FB8",
+    "#6A9589",
+    "#C8C093",
+  },
+
+  brights = {
+    "#727169",
+    "#B798F3",
+    "#98BB6C",
+    "#E6C384",
+    "#7FB4CA",
+    "#938AA9",
+    "#7AA89F",
+    "#DCD7BA",
+  },
+}
+
+return colorscheme
